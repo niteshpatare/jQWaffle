8ad29404a52a87149ead0c7158ce36e0b4922c8e
Nitesh.Patare <Nitesh.Patare@RapidCircle.com>
2018-04-16 13:01:56 +0530

    selector to variables
    coverted selectors to js variables, instead of interpolating them multiple times

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 
-      var navFlyMenu = $(".nav-flyMenu");
-      var adminFlyMenu = $(".admin-flyMenu");
-      var closeButton = $(".closeButton");
-      var dotsButton = $(".dotsButton");
-      var dotsButtonLanucher = $(".dotsButtonLanucher");
+      var $navFlyMenu = $(".nav-flyMenu");
+      var $adminFlyMenu = $(".admin-flyMenu");
+      var $closeButton = $(".closeButton");
+      var $dotsButton = $(".dotsButton");
+      var $dotsButtonLanucher = $(".dotsButtonLanucher");
+      var $navAppLauncher = $(".navMenuButton.appLauncher");
+      var $navDotsBtnLauncher = $(".navMenuButton.dotsButtonLanucher");
+
+
+      var $adminOpenButton = $(".userPersonalUrlLink");
+      var $adminCloseButton = $(".admin-flyMenu .menuClosebutton");
+
       var windowWidth = $(window).width();
       $(document).on("click",function(e)
       {
           e.stopPropagation();
-          var container = $(".nav-flyMenu");
+          let container = $($navFlyMenu);
           // if the target of the click isn't the container nor a descendant of the container
           if (!container.is(e.target) && container.has(e.target).length === 0 )
           {
@@ -17,76 +24,76 @@ $(document).ready(function(){
           }
       });
 
-    $(".navMenuButton.appLauncher, .navMenuButton.dotsButtonLanucher").on("click", function(e){
+    $( $navAppLauncher , $navDotsBtnLauncher).on("click", function(e){
       e.stopPropagation();
-      if(!navFlyMenu.hasClass("showMe")){
-          navFlyMenu.addClass("showMe");
-          navFlyMenu.removeClass("alwaysHidden");
+      if(!$navFlyMenu.hasClass("showMe")){
+          $navFlyMenu.addClass("showMe");
+          $navFlyMenu.removeClass("alwaysHidden");
           if(windowWidth <768){
-            adminFlyMenu.removeClass("showMe");
+            $adminFlyMenu.removeClass("showMe");
           }
           if(windowWidth >768 && windowWidth <=1024){
-            adminFlyMenu.removeClass("showMe");
+            $adminFlyMenu.removeClass("showMe");
           }
       }
-      else if(navFlyMenu.hasClass("showMe") && !navFlyMenu.hasClass("alwaysHidden") && windowWidth >768 ){
-          navFlyMenu.removeClass("showMe");
+      else if($navFlyMenu.hasClass("showMe") && !$navFlyMenu.hasClass("alwaysHidden") && windowWidth >768 ){
+          $navFlyMenu.removeClass("showMe");
       }
-      else if(navFlyMenu.hasClass("showMe") && adminFlyMenu.hasClass("showMe") && !navFlyMenu.hasClass("alwaysHidden") && windowWidth <768 ){
-            adminFlyMenu.removeClass("showMe");
+      else if($navFlyMenu.hasClass("showMe") && $adminFlyMenu.hasClass("showMe") && !$navFlyMenu.hasClass("alwaysHidden") && windowWidth <768 ){
+            $adminFlyMenu.removeClass("showMe");
       }
-      else if(navFlyMenu.hasClass("showMe") && !navFlyMenu.hasClass("alwaysHidden") ){
-          navFlyMenu.removeClass("showMe");
+      else if($navFlyMenu.hasClass("showMe") && !$navFlyMenu.hasClass("alwaysHidden") ){
+          $navFlyMenu.removeClass("showMe");
           if(windowWidth <768){
-            adminFlyMenu.removeClass("showMe");
+            $adminFlyMenu.removeClass("showMe");
           }
       }
       else{
-        navFlyMenu.removeClass("alwaysHidden");
+        $navFlyMenu.removeClass("alwaysHidden");
       }
 
     });
 
-    $(".userPersonalUrlLink, .admin-flyMenu .menuClosebutton").on("click", function(e){
+    $($adminOpenButton, $adminCloseButton).on("click", function(e){
       e.stopPropagation();
-      if(!adminFlyMenu.hasClass("showMe")){
-          adminFlyMenu.addClass("showMe");
+      if(!$adminFlyMenu.hasClass("showMe")){
+          $adminFlyMenu.addClass("showMe");
           if(windowWidth >768 && windowWidth <1024 ){
-              navFlyMenu.removeClass("showMe");
+              $navFlyMenu.removeClass("showMe");
           }
       }
-      else if(adminFlyMenu.hasClass("showMe") ){
-          adminFlyMenu.removeClass("showMe");
-          if(windowWidth <768 && closeButton.hasClass("showMe")){
+      else if($adminFlyMenu.hasClass("showMe") ){
+          $adminFlyMenu.removeClass("showMe");
+          if(windowWidth <768 && $closeButton.hasClass("showMe")){
 
           }
       }
 
     });
 
-    $(".dotsButton, .closeButton").on("click", function(e){
+    $($dotsButton , $closeButton).on("click", function(e){
       e.stopPropagation();
-      if(!closeButton.hasClass("showMe")){
-          closeButton.addClass("showMe");
-          dotsButton.addClass("hideMe");
-          dotsButtonLanucher.addClass("showMe");
-          navFlyMenu.removeClass("showMe");
+      if(!$closeButton.hasClass("showMe")){
+          $closeButton.addClass("showMe");
+          $dotsButton.addClass("hideMe");
+          $dotsButtonLanucher.addClass("showMe");
+          $navFlyMenu.removeClass("showMe");
       }
-      else if(closeButton.hasClass("showMe") ){  //&& !navFlyMenu.hasClass("alwaysHidden")
-          closeButton.removeClass("showMe");
-          dotsButton.removeClass("hideMe");
-          dotsButtonLanucher.removeClass("showMe");
-          navFlyMenu.removeClass("showMe");
+      else if($closeButton.hasClass("showMe") ){  //&& !$navFlyMenu.hasClass("alwaysHidden")
+          $closeButton.removeClass("showMe");
+          $dotsButton.removeClass("hideMe");
+          $dotsButtonLanucher.removeClass("showMe");
+          $navFlyMenu.removeClass("showMe");
       }
 
     });
 
      $( window ).resize(function() {
-      // if(navFlyMenu.hasClass("showMe") && !navFlyMenu.hasClass("alwaysHidden") && !closeButton.hasClass("showMe") && windowWidth <768 ){
-      if(navFlyMenu.hasClass("showMe") && !navFlyMenu.hasClass("alwaysHidden") && !$('.dotsButton:visible').length == 0 && windowWidth <768 ){
-           navFlyMenu.removeClass("showMe");
+      if($navFlyMenu.hasClass("showMe") && !$navFlyMenu.hasClass("alwaysHidden") && !$($dotsButton + ':visible').length == 0 && windowWidth <768 ){
+           $navFlyMenu.removeClass("showMe");
        }
      });
+
 
 
 });
